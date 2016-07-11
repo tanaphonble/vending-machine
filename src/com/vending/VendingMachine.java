4f1d9ba0cf9f60bc51bc28a70d98688a186b748f
outnotin <outnotin@gmail.com>
2016-07-11 15:19:12 +0700

    commit vending machine

--- src/com/vending/VendingMachine.java
+++ src/com/vending/VendingMachine.java
@@ -4,4 +4,16 @@ package com.vending;
  * Created by Tanaphon on 7/11/2016.
  */
 public class VendingMachine {
+
+    protected void insertCoin(){
+
+    }
+
+    protected void calculate(){
+
+    }
+
+    protected void returnItem(){
+
+    }
 }
